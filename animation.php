b4742634d223e472969f1891dc4df39215fb43e0
Dylan Nirvana <dylan@dylannirvana.com>
2021-10-06 15:13:27 -0400

    corrects animation resolution

---
+++ animation.php
@@ -0,0 +1,15 @@
+<!-- OUR LITTLE COMBINATION OF MEDIA CHOOSER AND RESOLUTION SWITCHING -->
+<picture class="logo" >
+  <!-- <source media="(max-width: 600px)" type="image/avif" srcset="assets/london480.avifs"> -->
+  <!-- <source media="(max-width: 600px)" type="image/webp" srcset="assets/london480.webp"> -->
+  <source media="(min-width: 601px)" type="image/avif" srcset="assets/london800b.avifs">
+  <source media="(min-width: 601px)" type="image/webp" srcset="assets/london800b.webp">
+  <!-- <source media="(min-width: 900px)" type="image/avif" srcset="assets/london1080.avif"> -->
+  <!-- <source media="(min-width: 900px)" type="image/webp" srcset="assets/london1080.webp"> -->
+  <!-- <img src="assets/london800.jpg" alt="BMI Awards London"> -->
+  <img src="assets/london800b.jpg" alt="BMI Awards London">
+  <!-- Resolution switcher works in localhost but media chooser MUST be tested in browserstack to see if it works! -->
+   
+</picture>
+
+<!-- background color is #16113f -->
