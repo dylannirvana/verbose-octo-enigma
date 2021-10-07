1aab598aa21bebc64d9d5e67f5779d6acaac76a4
Dylan Nirvana <dylan@dylannirvana.com>
2021-10-07 09:48:45 -0400

    correct markup for tagged img

--- animation.php
+++ animation.php
@@ -4,7 +4,7 @@
   <source media="(max-width: 600px)" type="image/webp" srcset="assets/london600.webp">
   <source media="(min-width: 601px)" type="image/avif" srcset="assets/london800.avif">
   <source media="(min-width: 601px)" type="image/webp" srcset="assets/london800.webp">
-  <source media="(min-width: 900px)" type="image/avif" srcset="assets/london1080.avif">
+  <source media="(min-width: 900px)" type="image/avif" srcset="assets/london1080_tag.avif">
   <source media="(min-width: 900px)" type="image/webp" srcset="assets/london1080.webp">
   <img src="assets/london800.jpg" alt="BMI Awards London">
   <!-- Resolution switcher works in localhost but media chooser MUST be tested in browserstack to see if it works! -->
